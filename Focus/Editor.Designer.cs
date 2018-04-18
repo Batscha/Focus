58866357949da6cecdde5d402b1b5da54f71739b
PC-05726C\Christopher <a@b.de>
2018-04-18 18:32:09 +0200

    Editor Backcolour

--- Focus/Editor.Designer.cs
+++ Focus/Editor.Designer.cs
@@ -395,6 +395,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Thistle;
             this.ClientSize = new System.Drawing.Size(997, 774);
             this.Controls.Add(this.labelGamma);
             this.Controls.Add(this.trackBarGamma);
